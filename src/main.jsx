140110f02dcc001ddeb70178c7323c664c31acb8
DataDrivenSilas <silasude811@gmail.com>
2025-04-07 05:43:44 +0100

    Added main.jsx and fixed index.html for Vite

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import LandlordDashboard from "./App";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <LandlordDashboard />
+  </React.StrictMode>
+);
